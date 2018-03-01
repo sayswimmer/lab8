a99d5422eb96e20aa2e37d5ffdeefd054eb98ffe
Sarah Yao <sayao@ucsd.edu>
2018-03-01 09:45:02 -0800

    thirdpartyAPI lab

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,17 @@
 function initMap() {
-	// add your code here
+	// create center marker at UCSD
+	var ucsd_ltlng = {lat:32.8788, lng:-117.2359};
+
+	// create a map object adn specify the DOM element for display
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: ucsd_ltlng,
+		zoom: 18
+	});
+
+	var marker = new google.maps.Marker({
+		position: ucsd_ltlng,
+		map: map,
+		title: "UCSD"
+	});
+
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -8,3 +8,14 @@ var data = {
 exports.adminView = function(req, res){
   res.render('rsvp', data);
 };
+
+exports.addRSVP = function(req, res) {
+  var rsvpEmail = req.body.rsvpEmail;
+  // this will print in the terminal when a POST is made
+  console.log(rsvpEmail);
+
+  data.rsvp.push(rsvpEmail); // Add to current data
+
+  // send our task name back so our browser knows it worked
+  res.send(rsvpEmail);
+}
